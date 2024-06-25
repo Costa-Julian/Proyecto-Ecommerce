fec8b6376bda8cf7900ee0754656f3321bfaf608
francisco191 <silfrancisco99@gmail.com>
2024-06-25 17:42:20 -0300

    Se comienza a desarrollar la modificación de producto

--- E-commerce/src/Pages/ModificarProducto.jsx
+++ E-commerce/src/Pages/ModificarProducto.jsx
@@ -1,73 +1,122 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { getProductById } from '../Services/productService';
+import { useNavigate } from 'react-router-dom';
 
-function buscarProductoPorId(array, idBuscado) {
-    if (array.length === 0) return null;
-    for (let i = 0; i < array.length; i++) {
-        if (array[i].id == idBuscado) {
-            return array[i]; 
-        }
-    }
-    return null; 
-}
-
-export const ModificarProducto = ({ productos,actualizarProducto }) => {
+export const ModificarProducto = () => {
 
     const { id } = useParams();
-    const item = buscarProductoPorId(productos,id)
+    const item = getProductById(id)
     const [productoModificado, setProductoModificado] = useState(item);
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setProductoModificado(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-    };
-    const handleGuardarCambios = () => {
-        actualizarProducto(productoModificado);
-    };
+    const [nombre, setNombre] = useState('');
+    const [descripcion, setDescripcion] = useState('');
+    const [precio, setPrecio] = useState('');
+    const [stock, setStock] = useState('');
+    const [categoriaId, setCategoriaId] = useState('');
+    const [categoriaNombre, setCategoriaNombre] = useState('');
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        getProductById(id).then(producto => {
+            setNombre(producto.nombre);
+            setDescripcion(producto.descripcion);
+            setPrecio(producto.precio);
+            setStock(producto.stock);
+            setCategoriaId(producto.categoria.id);
+            setCategoriaNombre(producto.categoria.nombre);
+        });
+    }, [id]);
+
+    const handleClick = () => {
+        const categoria = {
+            id: parseInt(categoriaId),
+            nombre: categoriaNombre
+        };
+        updateProduct(id, nombre, descripcion, precio, stock, categoria).then(() => {
+            navigate("/productos");
+        });
+    }
+
     return (
         <div className='container'>
             <h2>Modificar Producto</h2>
             <div className='row'>
                 <div class="col-6">
-                    <form class="contact__form" onSubmit={(e) => e.preventDefault()}>
-                        <div class="checkout__input">
-                            <label htmlFor="titulo">Título:</label>
+                <form className="contact__form" onSubmit={(e) => e.preventDefault()}>
+                        <div className="form-group">
+                            <label className="form-label">Nombre: </label>
                             <input
+                                value={nombre}
+                                onChange={(e) => setNombre(e.target.value)}
+                                className="form-control"
                                 type="text"
-                                id="titulo"
-                                name="title"
-                                value={productoModificado.title}
-                                onChange={handleChange}
+                                id="nombre"
+                                name="nombre"
                             />
                         </div>
-                        <div class="checkout__input">
-                            <label htmlFor="precio">Precio:</label>
+                        <div className="form-group">
+                            <label className="form-label">Descripción: </label>
                             <input
+                                value={descripcion}
+                                onChange={(e) => setDescripcion(e.target.value)}
+                                className="form-control"
+                                type="text"
+                                id="descripcion"
+                                name="descripcion"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label className="form-label">Precio: </label>
+                            <input
+                                value={precio}
+                                onChange={(e) => setPrecio(e.target.value)}
+                                className="form-control"
                                 type="number"
                                 id="precio"
-                                name="price"
-                                value={productoModificado.price}
-                                onChange={handleChange}
+                                name="precio"
                             />
                         </div>
-                        <div class="checkout__input">
-                            <label htmlFor="descripcion">Descripción:</label>
-                            <textarea
-                                id="descripcion"
-                                name="description"
-                                value={productoModificado.description}
-                                onChange={handleChange}
-                            ></textarea>
+                        <div className="form-group">
+                            <label className="form-label">Stock: </label>
+                            <input
+                                value={stock}
+                                onChange={(e) => setStock(e.target.value)}
+                                className="form-control"
+                                type="number"
+                                id="stock"
+                                name="stock"
+                            />
                         </div>
-                        <button className='primary-btn' onClick={handleGuardarCambios}>Guardar cambios</button>
+                         <div className="form-group">
+                            <label className="form-label">ID de Categoría: </label>
+                            <input
+                                value={categoriaId}
+                                onChange={(e) => setCategoriaId(e.target.value)}
+                                className="form-control"
+                                type="number"
+                                id="categoriaId"
+                                name="categoriaId"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label className="form-label">Nombre de Categoría: </label>
+                            <input
+                                value={categoriaNombre}
+                                onChange={(e) => setCategoriaNombre(e.target.value)}
+                                className="form-control"
+                                type="text"
+                                id="categoriaNombre"
+                                name="categoriaNombre"
+                            />
+                        </div>
+                        <button className='btn btn-primary' role="button" onClick={handleClick}>Actualizar producto</button>
                     </form>
                 </div>
                 <div class="col-6">
                     <img 
-                        src={productoModificado.image} 
+                        src="../public/vite.svg"
                         style={{ width: '100%', minHeight: '250px', maxHeight: '100%', objectFit: 'cover' }}  
                         alt=""
                     />
--- E-commerce/src/Services/productService.js
+++ E-commerce/src/Services/productService.js
@@ -11,7 +11,6 @@ export const getAllProductos = () => {
 }
 
 export const getProductById = (id) => {
-    console.log("ESTO TIENE EL ID ", id)
     return fetch(`http://localhost:4003/producto/${id}`)
         .then(response => {
             if (!response.ok) {
@@ -47,4 +46,32 @@ export const createProduct = (nombre, descripcion, precio, stock, categoria) => 
         .then(result => console.log(result))
         .catch(error => console.log('error', error));
 }
+
+export const updateProduct = (id, nombre, descripcion, precio, stock, categoria) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "nombre": nombre,
+        "descripcion": descripcion,
+        "precio": precio,
+        "stock": stock,
+        "categoria": categoria
+    });
+
+    var requestOptions = {
+        method: 'PUT',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch(`http://localhost:4003/producto/update/${id}`, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
+
+
+
 
